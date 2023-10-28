fe0f727363d196f72667c08c3b2cb45b7a074e26
Muhammadurasheed <yekinirasheed2002@gmail.com>
2023-10-28 19:20:53 +0100

    AI doctor refined

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
 
-Color scaffoldBackgroundColor = const Color(0xFF097969);
+Color scaffoldBackgroundColor = const Color(0xFF00C6AD);
 
-Color cardColor = const Color(0xFF097969);
+Color cardColor = const Color(0xFF00C6AD);
